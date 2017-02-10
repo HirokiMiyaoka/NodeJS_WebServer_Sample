923a4ad53fc2b98cdbc73c85f92ccbfb269aaeef
HirokiMiyaoka <hiroki.miyaoka@gmail.com>
2017-02-10 11:20:18 +0900

    Add files.

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # NodeJS_WebServer_Sample
+
+NodeJsにおける、Webサーバーのサンプルです。
+
+とりあえずドキュメントルート以下のファイルを読みこんで表示するのと、404ページの表示くらいはサポートしています。
+
+## サーバー起動
+
+```
+npm run start
+```
+
+## サーバー情報
+
+デフォルトは以下。
+
+* URL
+    * http://127.0.0.1:8090
+* ドキュメントルート
+    * public/
+
+## 設定
+
+`config.json` にもろもろの設定があるので、それで設定可能。
+
+## MIME Type
+
+`config.json` のmimeに拡張子に対応するMIME Typeを追加できます。
+
+```
+{
+	"docroot": "public",
+	"port": 8090,
+	"host": "127.0.0.1",
+	"mime": {
+		"png": "image/png",
+		"txt": "text/plain"
+	}
+}
+```
---
+++ server.js
@@ -0,0 +1,69 @@
+var http     = require( 'http' );
+var fs       = require( 'fs' );
+var path     = require( 'path' );
+
+var server   = http.createServer();
+var settings = require('./config.json');
+
+console.log( settings );
+
+var docroot = settings.docroot || 'public';
+
+var MIME =
+{
+	'css':	'text/css',
+	'gif':	'image/gif',
+	'gz':	'application/gzip',
+	'html':	'text/html',
+	'ico':	'image/x-icon',
+	'jpg':	'image/jpeg',
+	'js':	'text/javascript',
+	'png':	'image/png',
+	'txt':	'text/plain',
+	'zip':	'application/zip',
+};
+
+if ( settings.mime )
+{
+	Object.keys( settings.mime ).forEach( function( key )
+	{
+		MIME[ key ] = settings.mime[ key ];
+	} );
+}
+
+function e404( res )
+{
+	res.writeHead( 404, { 'Content-Type': 'text/plain' } );
+	res.write( '404: page not found.' );
+	res.end();
+}
+
+function fileExists( filepath )
+{
+	try
+	{
+		fs.statSync(filepath);
+		return true
+	} catch( err ) {}
+	return false;
+}
+
+server.on( 'request', function ( req, res )
+{
+	var filepath = req.url || '/';
+	if ( filepath.match( /\/$/ ) ) { $filepath += 'index.html'; }
+	filepath = path.join( docroot, filepath );
+
+	if ( !fileExists( filepath ) ) { return e404( res ); }
+
+	fs.readFile( filepath, 'utf-8', function( err, data )
+	{
+		if( err ) { return e404( res ); }
+		var extname = path.extname( filepath ).replace( '.', '' );
+		res.writeHead( 200, { 'Content-Type': MIME[ extname ] || 'text/plain' } );
+		res.write( data );
+		res.end();
+	} );
+} );
+
+server.listen( settings.port || 8090, settings.host || '127.0.0.1' );
